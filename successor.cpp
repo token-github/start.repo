0eec7b5c64ffb1d7f26fc2030793e70f4f9c1d4a
Robin Campos <rcampos@unm.edu>
2017-08-23 14:13:45 -0600

    This adds a successor operation

---
+++ successor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int element;
+	cout << getlin(cin, element) << endl;
+	return element + 1;
+}
